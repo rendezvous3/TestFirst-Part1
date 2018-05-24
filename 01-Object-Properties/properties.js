94543fc4386887f60fb070f0f9a448083f859927
rendezvous3 <bojanj6m@gmail.com>
2018-05-24 04:41:51 -0400

    properties

--- 01-Object-Properties/properties.js
+++ 01-Object-Properties/properties.js
@@ -1,28 +1,28 @@
-// function setPropsOnObj(obj){
-//   obj.x = 7
-//   obj.y = 8
-//   obj['onePlus'] = function(num) {
-//     return num + 1
-//   }
-// }
+function setPropsOnObj(obj){
+  obj.x = 7
+  obj.y = 8
+  obj['onePlus'] = function(num) {
+    return num + 1
+  }
+}
 
-// function setPropsOnArr(arr){
-//   arr.hello = function(){
-//     return 'Hello!'
-//   }
-//   arr.full = 'stack'
-//   arr[0] = 5
-//   arr.twoTimes = function(n){
-//     return 2 * n
-//   }
-// }
+function setPropsOnArr(arr){
+  arr.hello = function(){
+    return 'Hello!'
+  }
+  arr.full = 'stack'
+  arr[0] = 5
+  arr.twoTimes = function(n){
+    return 2 * n
+  }
+}
 
-// function setPropsOnFunc(fn){
-//   fn.year = '20??'
-//   fn.divideByTwo = function(n){
-//     return n / 2
-//   }
-//   fn.prototype.helloWorld = function(){
-//     return 'Hello World'
-//   }
-// }
+function setPropsOnFunc(fn){
+  fn.year = '20??'
+  fn.divideByTwo = function(n){
+    return n / 2
+  }
+  fn.prototype.helloWorld = function(){
+    return 'Hello World'
+  }
+}
