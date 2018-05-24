c929d249ca387b8f9f98d5526602116d226593dc
rendezvous3 <bojanj6m@gmail.com>
2018-05-24 04:41:18 -0400

    reading

--- 00-Reading-Test-Specs/reading.js
+++ 00-Reading-Test-Specs/reading.js
@@ -1,11 +1,11 @@
-// function hello(name){
-// 	if(typeof name === "undefined"){
-// 		return "Hello!";
-// 	} else {
-// 		return "Hello, " + name + "!";
-// 	}
-// }
+function hello(name){
+	if(typeof name === "undefined"){
+		return "Hello!";
+	} else {
+		return "Hello, " + name + "!";
+	}
+}
 
-// function add(number, num){
-// 	return number + num;
-// }
+function add(number, num){
+	return number + num;
+}
